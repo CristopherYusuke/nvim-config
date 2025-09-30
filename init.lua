29a69f65df5d20e42c2eb237908c6526d65acdf9
Cristopher Martins <cristopher.martins@glinttglobal.com>
2025-09-30 10:25:57 +0100

    lua plugins

--- init.lua
+++ init.lua
@@ -14,48 +14,9 @@ local rtp = vim.opt.rtp
 
 rtp:prepend(lazypath)
 require('lazy').setup( {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-  },
-  { 
- 'Mofiqul/vscode.nvim',
- lazy = false ,
- priority = 1000,
- config = function () 
-	vim.o.background = 'dark'
-	local c = require('vscode.colors').get_colors()
-	require('vscode').setup({ 
-	    italic_comments=true, 
-	    italic_comments = true,
-        italic_inlayhints = true,
-        underline_links = true,
-        disable_nvimtree_bg = true,
-        terminal_colors = true,
-        color_overrides = {
-            vscLineNumber = '#FFFFFF',
-        },
-        group_overrides = {
-            Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
-        }
-	})
-	vim.cmd.colorscheme "vscode"
-    end 
-  }
+  require 'plugins.neotree',
+  require 'plugins.bufferline',
+  require 'plugins.colortheme'
 }) 
-
-
-
-
-
-
-
-
 
 
