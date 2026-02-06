df39a06a2a22b3cd07eb5122d5dcb7b3d498b876
Cristopher Martins <cristopher.martins@glinttglobal.com>
2026-02-06 16:45:19 +0000

    fix neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,13 +1,22 @@
 return {
 	{
 		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hdie_dotfiles = false,
+				},
+			},
+		},
 		branch = "v3.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
 			"nvim-tree/nvim-web-devicons",
 		},
-		config = function()
+		config = function(_, opts)
+			require("neo-tree").setup(opts)
 			vim.keymap.set("n", "<leader>e", ":Neotree toggle position=left<CR>", { noremap = true, silent = true })
 		end,
 	},
